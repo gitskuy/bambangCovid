794d67b42cd8fbc62e5c1505566a8874767286d1
gitskuy <poleahmad@gmail.com>
2021-04-05 12:55:20 +0800

    fix procfile

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+class Bambang(Resource):
+    def get(self):
+        return {'hello': 'bambang!'}
+
+api.add_resource(HelloWorld, '/')
+api.add_resource(Bambang, '/BambangZ')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)

248ea293bcc4429a4aa40fea3aff2d01f25860ac
gitskuy <poleahmad@gmail.com>
2021-04-05 13:06:30 +0800

    fix procfile

--- app.py
+++ app.py
@@ -16,6 +16,4 @@ api.add_resource(HelloWorld, '/')
 api.add_resource(Bambang, '/BambangZ')
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+     app.run(threaded=True, port=5000)

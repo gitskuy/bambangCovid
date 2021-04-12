0e509bf7bf206840d1abcaef692a4b8df32b502d
ahmad.harharah <poleahmad@gmail.com>
2021-04-12 21:24:17 +0700

    new endpoint

--- app.py
+++ app.py
@@ -9,14 +9,25 @@ class HelloWorld(Resource):
     def get(self):
         return jsonify({'name': 'Hello',
                     'email': 'Hello World!'})
-
+    def put(self,user_id):
+       
 class Bambang(Resource):
     def get(self):
         return {'hello': 'bambang!'}
 
+class inGet(Resource):
+    def get(self):
+        return {"message": "Data Berhasil ditambahkan"}
+
+class outGet(Resource):
+    def get(self):
+        return {"message": "Data berhasil ditambahkan"}
 
 api.add_resource(HelloWorld, '/')
 api.add_resource(Bambang, '/BambangZ')
+api.add_resource(inGet, '/bambang/masuk')
+api.add_resource(Bambang, '/bambang/keluar')
+
 
 if __name__ == '__main__':
      app.run(threaded=True, port=5000)

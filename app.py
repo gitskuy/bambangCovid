de10cb8d097c7996559458a6285809e393bd2ef1
zaynnrth <zaynnrth@gmail.com>
2021-04-15 21:02:07 +0700

    Json Parsing from FE rev0.4

--- app.py
+++ app.py
@@ -10,25 +10,34 @@ class HelloWorld(Resource):
                     'email': 'Hello World!'})
 
 class inGet(Resource):
-        def get(self):
-        data = request.get_json(force=True)
-        nameCusto = data['Nama']
-        tgglCusto = data['TgglLr']
-        jeKelamin = data['JKel']
-        jeIdentit = data['JIden']
-        noIdentit = data['NoIden']
-        noTelepon = data['NoTel']
-        alamatKtp = data['AlamatKTP']
-        suhuBadan = data['Suhu']
-        provTujIn = data['provTujIn']
-        kebpTujIn = data['kebpTujIn']
-        kecmTujIn = data['kecmTujIn']
-        alamTujIn = data['alamTujIn']
-        provTujOu = data['provTujOu']
-        kabpTujOu = data['kabpTujOu']
-        kecmTujOu = data['kecmTujOu']
-        alamTujOu = data['alamTujOu']
-        return jsonify(json_data)
+    def get(self):
+        if data:
+            data = request.get_json(force=True)
+            nameCusto = data['nama_lengkap']
+            tgglCusto = data['tanggal_lahir']
+            jeKelamin = data['jenis_kelamin']
+            jeIdentit = data['jenis_identitas']
+            noIdentit = data['nomor_identitas']
+            noTelepon = data['no_telepon']
+            alamatKtp = data['alamat_sesuai_ktp']
+            suhuBadan = data['suhu_badan']
+            if data['tidak_ada_gejala'] == True:
+                dftrGjala = data['daftar_gejala']
+                gjalalain = data['gejala_lain']
+            else:
+                pass
+            kontatsta = data['is_kontak_positif']
+            provTujIn = data['provTujIn']
+            kebpTujIn = data['kebpTujIn']
+            kecmTujIn = data['kecmTujIn']
+            alamTujIn = data['alamTujIn']
+            provTujOu = data['provTujOu']
+            kabpTujOu = data['kabpTujOu']
+            kecmTujOu = data['kecmTujOu']
+            alamTujOu = data['alamTujOu']
+            return jsonify(data)
+        else:
+            return{"Message":"Data Tidak Ditemukan atau Gagal Parsing"}
 
 class outGet(Resource):
     def get(self):
@@ -58,7 +67,7 @@ class outGet(Resource):
             alamTujOu = data['alamTujOu']
             return jsonify(data)
         else:
-            return jsonify({'Message':'Data Tidak Ditemukan atau Gagal Parsing'})
+            return{"Message":"Data Tidak Ditemukan atau Gagal Parsing"}
 
 api.add_resource(HelloWorld, '/')
 api.add_resource(inGet, '/form-in')

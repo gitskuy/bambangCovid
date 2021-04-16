086396d2f871875ce7a492917339fe757bb7154c
zaynnrth <zaynnrth@gmail.com>
2021-04-16 10:23:27 +0700

    Json Parsing from FE rev1.1

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from flask import Flask, jsonify, request
 from flask_restful import reqparse, abort, Api, Resource
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/',methods=['GET','POST'])
-def home():
-    return jsonify({'Message': 'Semangat Memberantas Covid Bambang Covid!',
-                'email': 'bambangcovid@kenacovid.aw.com'})
+
+class HelloWorld(Resource):
+    def get(self):
+        return jsonify({'Message': 'Semangat Memberantas Covid Bambang Covid!',
+                    'email': 'bambangcovid@kenacovid.aw.com'})
 
 @app.route('/form-in',methods=['POST'])
 def formIn():
@@ -38,8 +40,8 @@ def formIn():
     except Exception as e:
         return {'error':str(e)}
 
-@app.route('/form-out',methods=['POST'])
-    def formOut():
+class outGet(Resource):
+    def get(self):
         try:
             data = request.get_json(force=True)
             nameCusto = data['nama_lengkap']
@@ -67,6 +69,11 @@ def formIn():
             return jsonify(data), 200
         except Exception as e:
             return {'error':str(e)}
+        
+
+api.add_resource(HelloWorld, '/')
+#api.add_resource(inGet, '/form-in')
+api.add_resource(outGet, '/form-out')
 
 if __name__ == '__main__':
      app.run(threaded=True, port=5000)

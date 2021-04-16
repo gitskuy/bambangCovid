9303a5dc1e2199594165c0681b8a1e40d305007b
zaynnrth <zaynnrth@gmail.com>
2021-04-16 10:13:12 +0700

    Json Parsing from FE rev1.0

--- app.py
+++ app.py
@@ -2,52 +2,44 @@ from flask import Flask, jsonify, request
 from flask_restful import reqparse, abort, Api, Resource
 
 app = Flask(__name__)
-api = Api(app)
 
+@app.route('/',methods=['GET','POST'])
+def home():
+    return jsonify({'Message': 'Semangat Memberantas Covid Bambang Covid!',
+                'email': 'bambangcovid@kenacovid.aw.com'})
 
+@app.route('/form-in',methods=['POST'])
+def formIn():
+    try:
+        data = request.get_json(force=True)
+        nameCusto = data['nama_lengkap']
+        tgglCusto = data['tanggal_lahir']
+        jeKelamin = data['jenis_kelamin']
+        jeIdentit = data['jenis_identitas']
+        noIdentit = data['nomor_identitas']
+        noTelepon = data['no_telepon']
+        alamatKtp = data['alamat_sesuai_ktp']
+        suhuBadan = data['suhu_badan']
+        if data['tidak_ada_gejala'] == False:
+            dftrGjala = data['daftar_gejala']
+            gjalalain = data['gejala_lain']
+        else:
+            pass
+        kontatsta = data['is_kontak_positif']
+        provTujIn = data['provTujIn']
+        kebpTujIn = data['kebpTujIn']
+        kecmTujIn = data['kecmTujIn']
+        alamTujIn = data['alamTujIn']
+        provTujOu = data['provTujOu']
+        kabpTujOu = data['kabpTujOu']
+        kecmTujOu = data['kecmTujOu']
+        alamTujOu = data['alamTujOu']
+        return jsonify(data), 200
+    except Exception as e:
+        return {'error':str(e)}
 
-class HelloWorld(Resource):
-    def get(self):
-        return jsonify({'Message': 'Semangat Memberantas Covid Bambang Covid!',
-                    'email': 'bambangcovid@kenacovid.aw.com'})
-
-@app.route('/form',methods=['POST'])
-def coba():
-    data = request.get_json(force=True)
-    return data['nama_lengkap']
-
-class inGet(Resource):
-    def get(self):
-        try:
-            data = request.get_json(force=True)
-            nameCusto = data['nama_lengkap']
-            tgglCusto = data['tanggal_lahir']
-            jeKelamin = data['jenis_kelamin']
-            jeIdentit = data['jenis_identitas']
-            noIdentit = data['nomor_identitas']
-            noTelepon = data['no_telepon']
-            alamatKtp = data['alamat_sesuai_ktp']
-            suhuBadan = data['suhu_badan']
-            if data['tidak_ada_gejala'] == False:
-                dftrGjala = data['daftar_gejala']
-                gjalalain = data['gejala_lain']
-            else:
-                pass
-            kontatsta = data['is_kontak_positif']
-            provTujIn = data['provTujIn']
-            kebpTujIn = data['kebpTujIn']
-            kecmTujIn = data['kecmTujIn']
-            alamTujIn = data['alamTujIn']
-            provTujOu = data['provTujOu']
-            kabpTujOu = data['kabpTujOu']
-            kecmTujOu = data['kecmTujOu']
-            alamTujOu = data['alamTujOu']
-            return jsonify(data), 200
-        except Exception as e:
-            return {'error':str(e)}
-
-class outGet(Resource):
-    def get(self):
+@app.route('/form-out',methods=['POST'])
+    def formOut():
         try:
             data = request.get_json(force=True)
             nameCusto = data['nama_lengkap']
@@ -75,11 +67,6 @@ class outGet(Resource):
             return jsonify(data), 200
         except Exception as e:
             return {'error':str(e)}
-        
-
-api.add_resource(HelloWorld, '/')
-api.add_resource(inGet, '/form-in')
-api.add_resource(outGet, '/form-out')
 
 if __name__ == '__main__':
      app.run(threaded=True, port=5000)
